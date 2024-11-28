3aaa5ccfa53fcba27c5fbca579b3401f41063c61
hamza <xiaoching666hz@gmail.com>
2024-11-28 01:35:53 +0100

    added the script of axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [api, setApi] = useState('');
+
+  useEffect(()=>{
+    axios.get(api).then((response)=>response.data).then((data)=>setData(data));
+  }, [api])
+  const handleapichange = (e) => {
+    setApi(e.target.value);
+  }
   return (
     <div className="App">
-
+        api link : <input type='text' onChange={handleapichange}/>
+        <p>data count is : {data.length}.</p>
     </div>
   );
 }
